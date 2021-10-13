a74af1b9fbf571c6533bb30a85765f6cd88ca563
Abel Souza <abelsouzacosta@gmail.com>
2021-10-13 13:50:35 -0300

    style(createuserrepository): create user
    Ensuire that with given values the repository must create an instance of user

--- src/infra/repositories/CreateUserRepository.spec.ts
+++ src/infra/repositories/CreateUserRepository.spec.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+import client from '../../client/client';
 import { MissingParamError } from '../../presentation/errors/MissingParamError';
 
 interface ICreateUser {
@@ -7,6 +9,12 @@ interface ICreateUser {
 }
 
 class CreateUserRepository {
+  private client: PrismaClient;
+
+  constructor(client: PrismaClient) {
+    this.client = client;
+  }
+
   async execute({ name, email, password }: ICreateUser) {
     if (!name || name === '') {
       throw new MissingParamError('name');
@@ -19,12 +27,26 @@ class CreateUserRepository {
     if (!password || password === '') {
       throw new MissingParamError('password');
     }
+
+    const user = this.client.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return user;
   }
 }
 
 describe('CreateUserRepository', () => {
+  beforeAll(async () => {
+    await client.user.deleteMany();
+  });
+
   it('Should throw a new MissingParamError if name is not provided', async () => {
-    const sut = new CreateUserRepository();
+    const sut = new CreateUserRepository(client);
     const promise = sut.execute({
       name: '',
       email: 'any_email',
@@ -35,7 +57,7 @@ describe('CreateUserRepository', () => {
   });
 
   it('Should throw a new MissingParamErorr if email is not provided', async () => {
-    const sut = new CreateUserRepository();
+    const sut = new CreateUserRepository(client);
     const promise = sut.execute({
       name: 'any_name',
       email: '',
@@ -46,7 +68,7 @@ describe('CreateUserRepository', () => {
   });
 
   it('Shold throw a new MissingParamError if password is not provided', async () => {
-    const sut = new CreateUserRepository();
+    const sut = new CreateUserRepository(client);
     const promise = sut.execute({
       name: 'any_name',
       email: 'any_email',
@@ -54,5 +76,21 @@ describe('CreateUserRepository', () => {
     });
 
     await expect(promise).rejects.toThrow(new MissingParamError('password'));
+  });
+
+  it('Should create an user instance with given params', async () => {
+    const sut = new CreateUserRepository(client);
+    const user = sut.execute({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
+
+    await expect(user).resolves.toEqual({
+      id: (await user).id,
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+    });
   });
 });
