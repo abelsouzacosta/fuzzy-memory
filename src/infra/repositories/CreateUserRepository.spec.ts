4d10c77467bb761c1f7c0285f8c508e3573a5448
Abel Souza <abelsouzacosta@gmail.com>
2021-10-13 13:36:45 -0300

    test(createuserservice): validation
    Ensure that execute method receives an email and a password

--- src/infra/repositories/CreateUserRepository.spec.ts
+++ src/infra/repositories/CreateUserRepository.spec.ts
@@ -11,6 +11,14 @@ class CreateUserRepository {
     if (!name || name === '') {
       throw new MissingParamError('name');
     }
+
+    if (!email || email === '') {
+      throw new MissingParamError('email');
+    }
+
+    if (!password || password === '') {
+      throw new MissingParamError('password');
+    }
   }
 }
 
@@ -24,5 +32,27 @@ describe('CreateUserRepository', () => {
     });
 
     await expect(promise).rejects.toThrow(new MissingParamError('name'));
+  });
+
+  it('Should throw a new MissingParamErorr if email is not provided', async () => {
+    const sut = new CreateUserRepository();
+    const promise = sut.execute({
+      name: 'any_name',
+      email: '',
+      password: 'any_password',
+    });
+
+    await expect(promise).rejects.toThrow(new MissingParamError('email'));
+  });
+
+  it('Shold throw a new MissingParamError if password is not provided', async () => {
+    const sut = new CreateUserRepository();
+    const promise = sut.execute({
+      name: 'any_name',
+      email: 'any_email',
+      password: '',
+    });
+
+    await expect(promise).rejects.toThrow(new MissingParamError('password'));
   });
 });
